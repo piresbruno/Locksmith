bd45db651da59c3298eb6ccd5c176554eb17eeec
Bruno Pires <bruno@blastersystems.com>
2016-06-08 11:45:21 +0100

    added support for iOS 7

--- Source/LocksmithAccessibleOption.swift
+++ Source/LocksmithAccessibleOption.swift
@@ -5,23 +5,42 @@ public enum LocksmithAccessibleOption: RawRepresentable {
     case WhenUnlocked, AfterFirstUnlock, Always, WhenUnlockedThisDeviceOnly, AfterFirstUnlockThisDeviceOnly, AlwaysThisDeviceOnly, WhenPasscodeSetThisDeviceOnly
     
     public init?(rawValue: String) {
-        switch rawValue {
-        case String(kSecAttrAccessibleWhenUnlocked):
-            self = WhenUnlocked
-        case String(kSecAttrAccessibleAfterFirstUnlock):
-            self = AfterFirstUnlock
-        case String(kSecAttrAccessibleAlways):
-            self = Always
-        case String(kSecAttrAccessibleWhenUnlockedThisDeviceOnly):
-            self = WhenUnlockedThisDeviceOnly
-        case String(kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly):
-            self = AfterFirstUnlockThisDeviceOnly
-        case String(kSecAttrAccessibleAlwaysThisDeviceOnly):
-            self = AlwaysThisDeviceOnly
-        case String(kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly):
-            self = WhenPasscodeSetThisDeviceOnly
-        default:
-            self = WhenUnlocked
+        if #available(iOS 8.0, *) {
+            switch rawValue {
+            case String(kSecAttrAccessibleWhenUnlocked):
+                self = WhenUnlocked
+            case String(kSecAttrAccessibleAfterFirstUnlock):
+                self = AfterFirstUnlock
+            case String(kSecAttrAccessibleAlways):
+                self = Always
+            case String(kSecAttrAccessibleWhenUnlockedThisDeviceOnly):
+                self = WhenUnlockedThisDeviceOnly
+            case String(kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly):
+                self = AfterFirstUnlockThisDeviceOnly
+            case String(kSecAttrAccessibleAlwaysThisDeviceOnly):
+                self = AlwaysThisDeviceOnly
+            case String(kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly):
+                self = WhenPasscodeSetThisDeviceOnly
+            default:
+                self = WhenUnlocked
+            }
+        } else {
+            switch rawValue {
+            case String(kSecAttrAccessibleWhenUnlocked):
+                self = WhenUnlocked
+            case String(kSecAttrAccessibleAfterFirstUnlock):
+                self = AfterFirstUnlock
+            case String(kSecAttrAccessibleAlways):
+                self = Always
+            case String(kSecAttrAccessibleWhenUnlockedThisDeviceOnly):
+                self = WhenUnlockedThisDeviceOnly
+            case String(kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly):
+                self = AfterFirstUnlockThisDeviceOnly
+            case String(kSecAttrAccessibleAlwaysThisDeviceOnly):
+                self = AlwaysThisDeviceOnly
+            default:
+                self = WhenUnlocked
+            }
         }
     }
     
@@ -34,7 +53,11 @@ public enum LocksmithAccessibleOption: RawRepresentable {
         case .Always:
             return String(kSecAttrAccessibleAlways)
         case .WhenPasscodeSetThisDeviceOnly:
-            return String(kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly)
+            if #available(iOS 8.0, *) {
+                return String(kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly)
+            } else {
+                return String(kSecAttrAccessibleWhenUnlocked)
+            }
         case .WhenUnlockedThisDeviceOnly:
             return String(kSecAttrAccessibleWhenUnlockedThisDeviceOnly)
         case .AfterFirstUnlockThisDeviceOnly:
